4d609d873a63a125225c41f574834bc58d8b739b
cjh <1418503647@qq.com>
2016-03-16 23:34:42 +0800

    封装了Bootstrap的alert

---
+++ src/eui.alert.js
@@ -0,0 +1,29 @@
+
+var qiao = {};
+qiao.bs 	= {};
+qiao.bs.msgoptions = {
+	msg  : 'msg',
+	type : 'info',
+	time : 2000,
+	position : 'top',
+};
+qiao.bs.msgstr = function(msg, type, position){
+	return '<div class="alert alert-'+type+' alert-dismissible" role="alert" style="display:none;position:fixed;' + position + ':0;left:0;width:100%;z-index:2001;margin:0;text-align:center;" id="bsalert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>'+msg+'</div>';
+};
+qiao.bs.msg = function(options){
+	var opt = $.extend({},qiao.bs.msgoptions);
+	
+	if(typeof options == 'string'){
+		opt.msg = options;
+	}else{
+		$.extend(opt, options);
+	}
+	
+	$('body').prepend(qiao.bs.msgstr(opt.msg, opt.type , opt.position));
+	$('#bsalert').slideDown();
+	setTimeout(function(){
+		$('#bsalert').slideUp(function(){
+			$('#bsalert').remove();
+		});
+	},opt.time);
+};
