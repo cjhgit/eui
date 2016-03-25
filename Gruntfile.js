977cf4d2bc4af51ea64b53bdf2dc2d5a5f14be41
cjh <1418503647@qq.com>
2016-03-25 15:33:30 +0800

    修改编码前备份

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,45 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
+        // 元数据
         pkg: grunt.file.readJSON('package.json'),
+        banner: '/*!\n' +
+                 ' * Bootstrap v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+                 ' * Copyright 2011-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+                 ' * Licensed under the <%= pkg.license %> license\n' +
+                 ' */\n',
+
+        // 任务配置
         uglify: {
             options: {
                 stripBanners: true,
                 banner: '/*! <%=pkg.name%>-<%=pkg.version%>.js <%=grunt.template.today("yyyy-mm-dd") %>*/\n'
             },
             build: {
-                src: 'src/test.js',
-                dest: 'build/<%=pkg.name%>-<%=pkg.version%>.min.js'
+                src: 'dist/js/eui.js',
+                dest: 'dist/js/eui.min.js'
             }
         },
         concat: {
             options: {
-                separator: ';'
+                separator: ';',
+                stripBanners: true,
+                banner: '/*! hello - v1.2.3 - 2014-2-4 */'
             },
             dist: {
                 src: [
+                    'src/js/affix.js',
+                    'src/js/alert.js',
+                    'src/js/button.js',
+                    'src/js/carousel.js',
+                    'src/js/collapse.js',
+                    'src/js/dropdown.js',
+                    'src/js/modal.js',
+                    'src/js/tooltip.js',
+                    'src/js/popover.js',
+                    'src/js/scrollspy.js',
+                    'src/js/tab.js',
+                    'src/js/transition.js',
                     'src/base.js',
                     'src/box/box.js',
                     'src/totop/totop.js',
@@ -32,14 +54,17 @@ module.exports = function (grunt) {
             },
             compress: {
                 files: {
-                    'dist/css/eui.css': [
+                    'dist/css/eui.min.css': [
+                        "dist/css/eui.css",
                         "src/base.css",
                         "src/box/box.css",
                         "src/totop/totop.css"
-                    ],
-                    'dist/css/bootstrap.min.css': [
-                        "dist/css/bootstrap.css"
                     ]
+                    /*
+                    'dist/css/eui.min.css': [
+
+                    ]
+                    */
                 }
             }
 
@@ -57,7 +82,7 @@ module.exports = function (grunt) {
                     yuicompress: false
                 },
                 files: {
-                    "dist/css/bootstrap.css": "less/bootstrap.less",
+                    "dist/css/eui.css": "src/less/bootstrap.less",
                     "src/index.css": "src/index.less"
                 }
             }
@@ -72,5 +97,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
 
     grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
-    grunt.registerTask('lessc',['less:task1', 'cssmin']);
+    grunt.registerTask('lessc',['less:task1', 'cssmin', 'concat', 'uglify']);
 };
