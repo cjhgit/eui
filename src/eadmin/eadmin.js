419d10ba4f1062d3be9327e024e8b2253469bf50
cjh <1418503647@qq.com>
2016-03-26 10:42:51 +0800

    备份

--- src/eadmin/eadmin.js
+++ src/eadmin/eadmin.js
@@ -1,3 +1,27 @@
 /**
  * Created by cjh1 on 2016/3/25.
  */
+
+$(document).ready(function(){
+    var thtmes = 'theme-default theme-blue theme-green';
+
+    $('#theme-blue').on('click', function(e){
+        e.preventDefault();
+
+        $('body').removeClass(thtmes);
+        $('body').addClass('theme-blue');
+    });
+
+    $('#theme-green').on('click', function(e){
+        e.preventDefault();
+
+        $('body').removeClass(thtmes);
+        $('body').addClass('theme-green');
+    });
+
+    $('#theme-default').on('click', function(e){
+        e.preventDefault();
+
+        $('body').removeClass(thtmes);
+    });
+});
